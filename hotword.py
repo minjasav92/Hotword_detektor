040cfee59f070af7ba6dfed23dcdbee0d36198d1
minjasav92 <minjcesav@gmail.com>
2026-06-29 14:51:59 +0200

    hotword promenjen u zub i prepoznaje razlike izmedju zub, zubic i zubar

--- hotword.py
+++ hotword.py
@@ -5,26 +5,17 @@ import matplotlib.pyplot as plt
 import IPython
 
 
-def promijeni_rate(wav, stari_rate, novi_rate):
-    broj_odbiraka = int(len(wav) * novi_rate / stari_rate)
-    return sig.resample(wav, broj_odbiraka)
-
-
-def ucitaj(putanja: str, ciljni_rate=16000):
-    rate, wav = wavfile.read(putanja)
-    if wav.ndim == 2:
-        wav = wav.mean(axis=1)
-    wav = promijeni_rate(wav, rate, ciljni_rate)
-    rate = ciljni_rate
-    print(f"frekvencija odabiranja - ", rate)
-    print(f"broj odabiraka", wav.shape)
-    return rate, wav
-
-def filtriraj_puter(snimak, rate, donji_prag=100, gornji_prag=7000, red=5):#ovi pragove sluze fakticki da ti vidis kju frekvenciju propustas a koju ne 
-
-    b, a = sig.butter(red, [donji_prag, gornji_prag], btype='band', fs=rate)#inicijalizujes parametre za taj filter jer postoje vise stepena tog filtera
-    filtriran = sig.lfilter(b, a, snimak)#pozivas filter sa parametrima da procisti signal
-
+def ucitaj(putanja, spusteni_rate=16000):
+    rate, wav=wavfile.read(putanja)
+    broj_odbiraka = int(len(wav) * spusteni_rate / rate)
+    rate=spusteni_rate
+    print(f"sample rate: ", rate)
+    print(f"broj odabiraka:",broj_odbiraka)
+    wav=sig.resample(wav, broj_odbiraka)
+    return rate,wav
+def filtriraj_puter(snimak, rate, donji_prag=100, gornji_prag=7000, red=5):
+    b, a=sig.butter(red,[donji_prag,gornji_prag],btype='band',fs=rate)
+    filtriran=sig.lfilter(b, a, snimak)
     fig, (ax1, ax2) = plt.subplots(1, 2, sharex="col", figsize=(10, 4), sharey="col")
     ax1.specgram(filtriran, Fs=rate)
     ax2.magnitude_spectrum(filtriran[:int(3*rate)], Fs=rate, scale='dB')
@@ -34,53 +25,46 @@ def filtriraj_puter(snimak, rate, donji_prag=100, gornji_prag=7000, red=5):#ovi 
     plt.show()
     return filtriran
 
-def uklanjanje_tisine(wav, rate, prag_trenutni=0.02, frejm_gledanja=30):
-    duzina_frejma = int(rate * frejm_gledanja / 1000)
-    broj_frejmova = len(wav) // duzina_frejma
-    energije = []
+def uklanjanje_tisine(wav, rate, prag_trenutni=0.1, frejm_gledanja=30):
+    duzina_frejma=int(rate*frejm_gledanja/1000)
+    broj_frejmova=len(wav)//duzina_frejma
+    energije=[]
     for i in range(broj_frejmova):
-        pocetak = i * duzina_frejma
-        kraj = (i + 1) * duzina_frejma
-        frejm = np.zeros(duzina_frejma)
+        pocetak=i*duzina_frejma
+        frejm=np.zeros(duzina_frejma)
         for j in range(duzina_frejma):
-            frejm[j] = wav[pocetak + j]
-        frejm = frejm.astype(np.float64)
-        energija = np.sum(frejm ** 2)
-        snaga = energija / len(frejm)
-        efektivna = np.sqrt(snaga)
+            frejm[j]=wav[pocetak+j]
+        frejm=frejm.astype(np.float64)
+        energija=np.sum(frejm**2)
+        snaga=energija/len(frejm)
+        efektivna=np.sqrt(snaga)
         energije.append(efektivna)
-    maks_energija = max(energije)
-
+    maks_energija=max(energije)
     rezultat = []
     for i, energija in enumerate(energije):
-        if energija >= prag_trenutni * maks_energija:
-            pocetak = i * duzina_frejma
+        if energija>=prag_trenutni*maks_energija:
+            pocetak=i*duzina_frejma
             for j in range(duzina_frejma):
-                rezultat.append(wav[pocetak + j])
-
-
+                rezultat.append(wav[pocetak+j])
     return np.array(rezultat)
-def predobradi(putanja: str, prikazi=True):
-    rate, wav = ucitaj(putanja)
-    filtriran = filtriraj_puter(wav, rate)
-    bez_tisine = uklanjanje_tisine(filtriran, rate)
-
-    print(f"len sa tision: {len(filtriran)}")
-    print(f"len bez tisine: {len(bez_tisine)}")
-
-    if prikazi:
-        fig, (ax1, ax2) = plt.subplots(1, 2, sharex="col", figsize=(10, 4), sharey="col")
-        ax1.specgram(bez_tisine, Fs=rate)
-        ax1.set_title("Spektrogram (final)")
-        ax2.magnitude_spectrum(bez_tisine, Fs=rate, scale='dB')
-        ax2.set_ylim(-120, 70)
-        ax2.grid()
-        plt.show()
-        IPython.display.Audio(data=bez_tisine, rate=rate)
+def predobradi(putanja):
+    rate, wav=ucitaj(putanja)
+    filtriran=filtriraj_puter(wav, rate)
+    bez_tisine=uklanjanje_tisine(filtriran, rate)
+    print(f"len sa tision: " , len(filtriran))
+    print(f"len bez tisine: ", len(bez_tisine))
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex="col", figsize=(10, 4), sharey="col")
+    ax1.specgram(bez_tisine, Fs=rate)
+    ax1.set_title("Spektrogram")
+    ax2.magnitude_spectrum(bez_tisine, Fs=rate, scale='dB')
+    ax2.set_ylim(-120, 70)
+    ax2.grid()
+    plt.show()
+    IPython.display.Audio(data=bez_tisine, rate=rate)
 
     return rate, bez_tisine
 def DTW(spektar_hotword, spektar_recenica):
-    visina, sirina=spektar_hotword.shape[1],spektar_recenica.shape[1]#rec, prozor
+    visina, sirina=spektar_hotword.shape[1],spektar_recenica.shape[1]
     frekvencije=spektar_hotword.shape[0]
     matrica =[]
     matrica=np.zeros((visina, sirina))
@@ -99,8 +83,8 @@ def DTW(spektar_hotword, spektar_recenica):
     for i in range (1, visina):
         for j in range(1, sirina):
             matrica_put[i,j]=matrica[i,j]+min(matrica_put[i-1,j], matrica_put[i-1, j-1], matrica_put[i,j-1])
-    return matrica_put[visina-1, sirina-1]/(sirina+visina)#aproksimira distancu jer u zavisnoti od audia imamo vie frejmova
-def trazenje_sekunde(hotword, recenica, rate_hotword,rate_recenica, prag=100):
+    return matrica_put[visina-1, sirina-1]/(sirina+visina)
+def trazenje_sekunde(hotword, recenica, rate_hotword,rate_recenica, prag=11800):
     duzina_prozora=len(hotword)
     korak=int(0.1*rate_recenica)
     distance=[]
@@ -114,20 +98,39 @@ def trazenje_sekunde(hotword, recenica, rate_hotword,rate_recenica, prag=100):
         d=DTW(spektar_hotword, spektar_frame)
         distance.append(d)
         vremena.append(i/rate_recenica)
-    return vremena, distance
-def rezultat(vreme, distance):
-    minimun=distance[0]
-    trenutak=-1
-    for i in range (len(distance)):
-        if minimun>distance[i]:
-            minimun=distance[i]
-            trenutak=vreme[i]
-    return trenutak
-if __name__ == '__main__':
-    rate1, wav1 = predobradi("hotword-mono.wav")
-    rate2, wav2 = ucitaj("recenia-mono.wav")
-    filtirana_recenica=filtriraj_puter(wav2, rate2)
-    
-    vreme, distance= trazenje_sekunde(wav1,filtirana_recenica, rate1, rate2)
-    print ("Hotword je detektovan u trenutku", rezultat(vreme, distance))
+    kandidati = []
+    print ("minimalna distanca" , min(distance))
+    for i in range(len(distance)):
+        if distance[i]<prag:
+            kandidati.append((vremena[i], distance[i]))
+    if len(kandidati)==0:
+        return -1
+    rezultati=[]
+    i=0
+    while i<len(kandidati):
+        grupa_vreme=[kandidati[i][0]]
+        grupa_dist=[kandidati[i][1]]
+        j =i+1
+        while j<len(kandidati) and (kandidati[j][0]-kandidati[j-1][0])<0.25:
+            grupa_vreme.append(kandidati[j][0])
+            grupa_dist.append(kandidati[j][1])
+            j+=1
+        najbolji_idx = grupa_dist.index(min(grupa_dist))
+        rezultati.append(grupa_vreme[najbolji_idx])
+        i=j
+
+    return rezultati
+
+rate1, wav1 = predobradi("z.wav")#hotword 
+rate2, wav2 = ucitaj("mnozinazuba.wav")#za recenicu
+#razlikuju se posto se za rec uklanja tisina a za recenicu ne
+filtirana_recenica=filtriraj_puter(wav2, rate2)
+lista_trenutaka= trazenje_sekunde(wav1,filtirana_recenica, rate1, rate2, prag=37700)
+if lista_trenutaka==-1:
+   print("Hotword nije detektovan")
+else :
+    print("Hotword je detektovan ",len(lista_trenutaka), "puta\ntrenuci detektovanja :")
+    for i in range(len(lista_trenutaka)):
+        print (lista_trenutaka[i], " s")
+
    
